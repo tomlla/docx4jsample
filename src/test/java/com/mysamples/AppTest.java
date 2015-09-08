d3a9f957e3d424f26a714d9f0fd2e59c43ee6f11
nobuyasu tomura <nobuyasu.tomura@soliton.co.jp>
2015-09-09 05:43:01 +0900

    update

--- src/test/java/com/mysamples/AppTest.java
+++ src/test/java/com/mysamples/AppTest.java
@@ -1,25 +1,69 @@
 package com.mysamples;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.apache.commons.io.FilenameUtils;
+import org.docx4j.Docx4J;
+import org.docx4j.convert.out.FOSettings;
+import org.docx4j.fonts.IdentityPlusMapper;
+import org.docx4j.fonts.PhysicalFont;
+import org.docx4j.fonts.PhysicalFonts;
+import org.docx4j.openpackaging.exceptions.Docx4JException;
+import org.docx4j.openpackaging.packages.WordprocessingMLPackage;
+import org.junit.Test;
 
-public class AppTest extends TestCase
-{
-    public AppTest( String testName ) {
-        super( testName );
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.util.function.Function;
+import java.util.regex.Pattern;
+import java.util.stream.IntStream;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.junit.Assert.assertThat;
+
+public class AppTest {
+
+    public static File prjRoot() {
+        return new File(System.getProperty("user.dir"));
     }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
+    @Test
+    public void testPrjRoot() {
+        assertThat(prjRoot().getAbsolutePath(),
+                is("/Users/ntomura1/ghqroot/github.com/tomlla/docx4jsample"));
     }
 
-    public void testApp()
-    {
-        assertTrue( true );
+    @Test
+    public void testApp() throws Docx4JException, FileNotFoundException {
+        final File srcDocxFile = new File(prjRoot(), "testfiles/LSM-1408A-SID-2810-S.docx");
+        final File outputPdf = new File(prjRoot(), "test-results/" +
+                FilenameUtils.getBaseName(srcDocxFile.getName()) + ".pdf");
+
+        final WordprocessingMLPackage wmlp = WordprocessingMLPackage.load(srcDocxFile); // throws Docx4JException
+        final IdentityPlusMapper fontMap = new IdentityPlusMapper();
+        try {
+            wmlp.setFontMapper(fontMap);
+        }catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        PhysicalFont pf = PhysicalFonts.get("");
+
+        final FOSettings foSettings = Docx4J.createFOSettings();
+        foSettings.setWmlPackage(wmlp);
+
+        final FileOutputStream fos = new FileOutputStream(outputPdf); // throws FileNotFoundException
+        Docx4J.toFO(foSettings, fos, Docx4J.FLAG_EXPORT_PREFER_XSL);
+    }
+
+    static <B> B repeated(Function<Integer,B> f, int n) {
+        for (int i = 0; i < n; i++) {
+
+        }
+        return null;
+    }
+
+    @Test
+    public void testRepeated() {
+
     }
 }
